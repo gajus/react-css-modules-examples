ee455dc5eef3fa75aba60a772e740555cf3eac0c
Gajus Kuizinas <gk@anuary.com>
2015-08-26 15:00:39 +0100

    Example using ES7 decorators.

---
+++ src/UsingDecorator/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './car.css';
+import CSSModules from 'react-css-modules';
+
+@CSSModules(styles)
+export default class extends React.Component {
+    render () {
+        return <div className='car'>
+            <div className='front-door'>front-door</div>
+            <div className='back-door'>back-door</div>
+        </div>;
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Car from './Car';
+import UsingDecorator from './UsingDecorator';
 
 ReactDOM.render(<div>
     <Car />
+    <UsingDecorator />
 </div>, document.querySelector('#app'));

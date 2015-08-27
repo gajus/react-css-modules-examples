1a9607d3bd54391ff803ab5c253aa90a819f34de
Gajus Kuizinas <gk@anuary.com>
2015-08-27 11:44:36 +0100

    Using the latest react-css-modules module.

--- src/UsingDecorator/index.js
+++ src/UsingDecorator/index.js
@@ -5,9 +5,9 @@ import CSSModules from 'react-css-modules';
 @CSSModules(styles)
 export default class extends React.Component {
     render () {
-        return <div className='car'>
-            <div className='front-door'>front-door</div>
-            <div className='back-door'>back-door</div>
+        return <div localClassName='car'>
+            <div localClassName='front-door'>front-door</div>
+            <div localClassName='back-door'>back-door</div>
         </div>;
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Car from './Car';
+import UsingLocalClassName from './UsingLocalClassName';
 import UsingDecorator from './UsingDecorator';
-import UsingModuleName from './UsingModuleName';
 
 ReactDOM.render(<div>
-    <Car />
+    <UsingLocalClassName />
     <UsingDecorator />
-    <UsingModuleName />
 </div>, document.querySelector('#app'));
